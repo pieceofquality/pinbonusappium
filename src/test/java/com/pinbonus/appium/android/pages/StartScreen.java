9191186cae63dfad0f459d400c30e06f681bf56c
Kirill Remizov <pieceofquality@gmail.com>
2016-05-21 12:16:56 +0300

    first

---
+++ src/test/java/com/pinbonus/appium/android/pages/StartScreen.java
@@ -0,0 +1,11 @@
+package com.pinbonus.appium.android.pages;
+
+import org.junit.Test;
+
+public class StartScreen {
+
+    @Test
+    public void screenSliding(){
+        
+    }
+}
--- src/test/java/com/pinbonus/appium/android/scenarios/AndroidSetup.java
+++ src/test/java/com/pinbonus/appium/android/scenarios/AndroidSetup.java
@@ -8,22 +8,21 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-
 public class AndroidSetup {
     protected AndroidDriver driver;
 
     protected void prepareAndroidForAppium() throws MalformedURLException {
-        File appDir = new File("/Users/nishant/Development/AppiumDemo/apps");
+        File appDir = new File("D:/Projects/pinbonusappium/apps");
         File app = new File(appDir, "pinbonus.apk");
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("device","Android");
+        capabilities.setCapability("device", "Android");
 
         //mandatory capabilities
-        capabilities.setCapability("deviceName","Android");
-        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName", "Android");
+        capabilities.setCapability("platformName", "Android");
 
         //other caps
         capabilities.setCapability("app", app.getAbsolutePath());
-        driver =  new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 }
