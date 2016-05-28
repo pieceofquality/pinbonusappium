50f91808c0dda50ebe3294dee17f09a4063a0d5c
Kirill Remizov <pieceofquality@gmail.com>
2016-05-28 18:57:28 +0300

    some changes

---
+++ src/test/java/com/pinbonus/appium/android/pages/WelcomeScreensPage.java
@@ -0,0 +1,27 @@
+package com.pinbonus.appium.android.pages;
+
+/**
+ * Created by piece on 28.05.2016.
+ */
+//public class WelcomeScreensPage {
+//
+//    public WelcomeScreensPage(AndroidDriver<WebElement> driver){
+//        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+//    }
+//
+//    @AndroidFindBy(id = "tvPromoText")
+//    public WebElement promoText;
+//
+//        @AndroidFindBy(id = "tvPromoText")
+//    public WebElement promoText;
+//
+//        @AndroidFindBy(id = "tvPromoText")
+//    public WebElement promoText;
+//
+//        @AndroidFindBy(id = "tvPromoText")
+//    public WebElement promoText;
+//
+//
+//
+//
+//}
